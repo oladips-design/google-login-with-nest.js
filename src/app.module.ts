63ac71e09f086f852d28f29922b0771969b74f74
noobDEV0007 <oladeleoladipupo555@gmail.com>
2024-01-16 12:26:35 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { User } from './typeorm/entities/User';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+    PassportModule.register({ session: true }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
